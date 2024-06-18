3c2a9b995800b4a4d47f4c5c34d7886d251e4d6b
Jared Ryan Gibson <jared_gibson@byu.edu>
2024-06-18 09:06:57 -0600

    Fix overly ambitious auth requirement

--- src/contexts/forms/forms-context.tsx
+++ src/contexts/forms/forms-context.tsx
@@ -5,7 +5,6 @@ import { Field, FieldGroup, Form } from "../../types/entities";
 import EditField from "../../components/forms/builder/EditField";
 import EditFieldGroup from "../../components/forms/builder/EditFieldGroup";
 import { ImmerReducer, useImmerReducer } from "use-immer";
-import { withAuthenticationRequired } from "../AuthProvider";
 
 export interface FormsState {
   activeForm?: Partial<Form>;
@@ -68,57 +67,58 @@ const formsReducer: ImmerReducer<FormsState, FormsAction> = (state, action) => {
   }
 };
 
-export const FormsContextProvider: React.FC<PropsWithChildren> =
-  withAuthenticationRequired(({ children }) => {
-    const [state, dispatch] = useImmerReducer(formsReducer, INITIAL_STATE);
+export const FormsContextProvider: React.FC<PropsWithChildren> = ({
+  children,
+}) => {
+  const [state, dispatch] = useImmerReducer(formsReducer, INITIAL_STATE);
 
-    const handleEditField = (field?: Partial<Field>) => {
-      dispatch({ type: "SET_ACTIVE_FIELD", payload: field });
-      dispatch({ type: "SET_EDIT_FIELD_SLIDER_OPEN", payload: true });
-    };
+  const handleEditField = (field?: Partial<Field>) => {
+    dispatch({ type: "SET_ACTIVE_FIELD", payload: field });
+    dispatch({ type: "SET_EDIT_FIELD_SLIDER_OPEN", payload: true });
+  };
 
-    const handleEditFieldGroup = (fieldGroup?: Partial<FieldGroup>) => {
-      dispatch({ type: "SET_ACTIVE_FIELD_GROUP", payload: fieldGroup });
-      dispatch({ type: "SET_EDIT_FIELD_GROUP_SLIDER_OPEN", payload: true });
-    };
+  const handleEditFieldGroup = (fieldGroup?: Partial<FieldGroup>) => {
+    dispatch({ type: "SET_ACTIVE_FIELD_GROUP", payload: fieldGroup });
+    dispatch({ type: "SET_EDIT_FIELD_GROUP_SLIDER_OPEN", payload: true });
+  };
 
-    return (
-      <FormsContext.Provider
-        value={{
-          state,
-          dispatch,
-          handleEditField,
-          handleEditFieldGroup,
-        }}
+  return (
+    <FormsContext.Provider
+      value={{
+        state,
+        dispatch,
+        handleEditField,
+        handleEditFieldGroup,
+      }}
+    >
+      {children}
+      <SlideOver
+        open={!!state.metadataSliderOpen}
+        setOpen={(open) =>
+          dispatch({ type: "SET_METADATA_SLIDER_OPEN", payload: open })
+        }
       >
-        {children}
-        <SlideOver
-          open={!!state.metadataSliderOpen}
-          setOpen={(open) =>
-            dispatch({ type: "SET_METADATA_SLIDER_OPEN", payload: open })
-          }
-        >
-          <EditFormMetadata />
-        </SlideOver>
-        <SlideOver
-          open={!!state.editFieldSliderOpen}
-          setOpen={(open) =>
-            dispatch({ type: "SET_EDIT_FIELD_SLIDER_OPEN", payload: open })
-          }
-        >
-          <EditField />
-        </SlideOver>
-        <SlideOver
-          open={!!state.editFieldGroupSliderOpen}
-          setOpen={(open) =>
-            dispatch({
-              type: "SET_EDIT_FIELD_GROUP_SLIDER_OPEN",
-              payload: open,
-            })
-          }
-        >
-          <EditFieldGroup />
-        </SlideOver>
-      </FormsContext.Provider>
-    );
-  });
+        <EditFormMetadata />
+      </SlideOver>
+      <SlideOver
+        open={!!state.editFieldSliderOpen}
+        setOpen={(open) =>
+          dispatch({ type: "SET_EDIT_FIELD_SLIDER_OPEN", payload: open })
+        }
+      >
+        <EditField />
+      </SlideOver>
+      <SlideOver
+        open={!!state.editFieldGroupSliderOpen}
+        setOpen={(open) =>
+          dispatch({
+            type: "SET_EDIT_FIELD_GROUP_SLIDER_OPEN",
+            payload: open,
+          })
+        }
+      >
+        <EditFieldGroup />
+      </SlideOver>
+    </FormsContext.Provider>
+  );
+};
