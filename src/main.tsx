b5456837f474b730b7b556a1ff00e26a5161a9ce
Jared Gibson <jared.gibson@live.com>
2024-12-03 08:39:46 -0700

    Fix broken import

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
+import { RouterProvider } from "react-router/dom";
+import { router } from "./router";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
